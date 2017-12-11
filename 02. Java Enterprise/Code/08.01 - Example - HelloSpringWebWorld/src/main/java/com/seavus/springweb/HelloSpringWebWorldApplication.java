17c7d270347654cce115a8ebd60a4a2f33a9dcfd
nikola.zivkov <nikola.zivkov@seavus.com>
2017-12-11 12:39:57 +0100

    Day 6.

---
+++ 02. Java Enterprise/Code/08.01 - Example - HelloSpringWebWorld/src/main/java/com/seavus/springweb/HelloSpringWebWorldApplication.java
@@ -0,0 +1,12 @@
+package com.seavus.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringWebWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringWebWorldApplication.class);
+    }
+}
